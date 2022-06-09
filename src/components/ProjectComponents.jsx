51ea18a8ba71c7b5f2d5a9e95938edbb7e1d77d1
manik1848 <manikprakash1848@gmail.com>
2022-06-09 19:17:51 +0530

    Update ProjectComponents.jsx

--- src/components/ProjectComponents.jsx
+++ src/components/ProjectComponents.jsx
@@ -26,7 +26,7 @@ const project3 = ["fab fa-html5","fab fa-css3-alt","fab fa-js","fab fa-react"]
         title="Bath&Body Works clone"
         description="In this project we have tried to make a look alike clone of Bath&Body Works website some of the functionality we have added are home page, payment page, slider, navbar, sorting, drop down,search by name inside the product pages, coupon functionality"
         arr={project2}
-        github="https://github.com/Arfeen98/Bathanbodyworks-clone"
+        github="https://github.com/manik1848/Bathanbodyworks-clone"
         website=" https://arfeen98.github.io/Bathanbodyworks-clone"
       />
       <Projects
@@ -34,7 +34,7 @@ const project3 = ["fab fa-html5","fab fa-css3-alt","fab fa-js","fab fa-react"]
       title="Orbitz Clone"
       description="In this project we have tried to make a look alike clone of Orbitz website with the tech stack React,Redux,CSS. We have try to made all the pages with the functionality of slider, sorting, drop down, user-authentication, we have created the data using local storage"
       arr={project3}
-      github="https://github.com/KalashThakur/Orbitz/tree/main/CW%20PROJECT/orbitz-clone-master"
+      github="https://github.com/manik1848/Orbitz"
       website="https://enchanting-creponne-8be7a1.netlify.app/"
 
       />
